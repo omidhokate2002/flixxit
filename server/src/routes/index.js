97641f639bf5473ddcb852e8952e5234b042ebe2
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-07-15 20:20:11 +0530

    Creeated signup and signin form and some custom changes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -7,8 +7,8 @@ import reviewRoute from "./review.router.js";
 const router = express.Router();
 
 router.use("/user", userRoute);
-router.use("/:mediaType", mediaRoute);
 router.use("/person", personRoute);
 router.use("/reviews", reviewRoute);
+router.use("/:mediaType", mediaRoute);
 
 export default router;
