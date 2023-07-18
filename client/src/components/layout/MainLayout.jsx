5255dfc0fa5b7029586859660ae911127c496553
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-07-18 15:36:58 +0530

    Creates some ui contents and sections for medias

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -5,8 +5,41 @@ import GlobalLoading from "../common/GlobalLoading.jsx";
 import Footer from "../common/Footer.jsx";
 import Topbar from "../common/Topbar.jsx";
 import AuthModal from "../common/AuthModal.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import userApi from "../../api/modules/user.api";
+import favoriteApi from "../../api/modules/favorite.api";
+import { setListFavorites, setUser } from "../../redux/features/userSlice";
 
 function MainLayout() {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const authUser = async () => {
+      const { response, err } = await userApi.getInfo();
+
+      if (response) dispatch(setUser(response));
+      if (err) dispatch(setUser(null));
+    };
+
+    authUser();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const getFavorites = async () => {
+      const { response, err } = await favoriteApi.getList();
+
+      if (response) dispatch(setListFavorites(response));
+      if (err) toast.error(err.message);
+    };
+
+    if (user) getFavorites();
+    if (!user) dispatch(setListFavorites([]));
+  }, [user, dispatch]);
+
   return (
     <>
       {/* global-loading */}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
-// import HeroSlide from "../components/common/HeroSlide";
-// import tmdbConfigs from "../api/configs/tmdb.configs";
-// import { Box } from "@mui/material";
-// import uiConfigs from "../configs/ui.configs";
-// import Container from "../components/common/Container";
-// import MediaSlide from "../components/common/MediaSlide";
+import React from "react";
+import HeroSlide from "../components/common/HeroSlide";
+import tmdbConfigs from "../api/configs/tmdb.configs";
+import { Box } from "@mui/material";
+import uiConfigs from "../configs/ui.configs";
+import Container from "../components/common/Container.jsx";
+import MediaSlide from "../components/common/MediaSlide";
 
 const HomePage = () => {
   return (
     <>
-      {/* <HeroSlide
+      <HeroSlide
         mediaType={tmdbConfigs.mediaType.movie}
         mediaCategory={tmdbConfigs.mediaCategory.popular}
       />
@@ -42,7 +42,7 @@ const HomePage = () => {
             mediaCategory={tmdbConfigs.mediaCategory.top_rated}
           />
         </Container>
-      </Box> */}
+      </Box>
     </>
   );
 };
