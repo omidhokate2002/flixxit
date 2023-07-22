50ccb4a26902eff42054bd56adbb98eca6990b00
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-07-22 23:43:56 +0530

    baseurl changed

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-// const baseURL = "http://127.0.0.1:5000/api/v1/";
-const baseURL = "https://flixxit-backend.vercel.app/api/v1/";
+const baseURL = "http://127.0.0.1:5000/api/v1/";
+// const baseURL = "https://flixxit-backend.vercel.app/api/v1/";
 // const baseURL = "https://flixxit.vercel.app/api/v1/";
 
 const privateClient = axios.create({
