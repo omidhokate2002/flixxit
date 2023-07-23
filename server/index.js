c29259c6003cfb913704a4c6e445482c65186f32
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-07-23 12:35:29 +0530

    fix some mongo bug

--- server/index.js
+++ server/index.js
@@ -29,6 +29,8 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+mongoose.set("strictQuery", false);
+
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
