2ff931a0b12a185244b83843b0a387b1901d266a
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-07-23 10:08:50 +0530

    add paths

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ import routes from "./src/routes/index.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000/", "https://flixit-client.vercel.app/"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
