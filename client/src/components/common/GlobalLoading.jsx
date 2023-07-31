9500309af1b4cf2089825badc509741c500e78b9
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-07-31 15:38:01 +0530

    Logo fixed in GlobalLoading page

--- client/src/components/common/GlobalLoading.jsx
+++ client/src/components/common/GlobalLoading.jsx
@@ -38,7 +38,7 @@ const GlobalLoading = () => {
             position: "absolute",
             top: "50%",
             left: "50%",
-            transform: "translate(-50%,-%50%",
+            transform: "translate(-50%, -50%)",
           }}
         >
           <Logo />
