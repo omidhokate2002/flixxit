a52d5af4708ce0c04cc75467802afb44186b2b86
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-07-29 18:03:53 +0530

    fix

--- client/src/components/common/MediaVideosSlide.jsx
+++ client/src/components/common/MediaVideosSlide.jsx
@@ -1,40 +1,34 @@
+import { Box } from "@mui/material";
 import { useEffect, useRef } from "react";
 import { SwiperSlide } from "swiper/react";
 import tmdbConfigs from "../../api/configs/tmdb.configs";
 import NavigationSwiper from "./NavigationSwiper";
-import { Box, Button } from "@mui/material";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const MediaVideo = ({ video }) => {
   const iframeRef = useRef();
 
   useEffect(() => {
-    const height = (iframeRef.current.offsetWidth * 9) / 16 + "px";
-    iframeRef.current.setAttribute("height", height);
+    if (video) {
+      const height = (iframeRef.current.offsetWidth * 9) / 16 + "px";
+      iframeRef.current.setAttribute("height", height);
+    }
   }, [video]);
 
   return (
     <Box sx={{ height: "max-content" }}>
-      <iframe
-        key={video.key}
-        src={tmdbConfigs.youtubePath(video.key)}
-        ref={iframeRef}
-        width="100%"
-        title={video.id}
-        style={{ border: 0 }}
-      ></iframe>
-      <div className="play-pause-button">
-        <Button
-          variant="contained"
-          sx={{ width: "max-content" }}
-          size="large"
-          startIcon={<PlayArrowIcon />}
-          id="fullscreen-button"
-          onClick={() => iframeRef.current.requestFullscreen()}
-        >
-          Fullscreen
-        </Button>
-      </div>
+      {video && (
+        <>
+          <iframe
+            key={video.key}
+            src={`${tmdbConfigs.youtubePath(video.key)}?controls=1`}
+            ref={iframeRef}
+            width="100%"
+            title={video.id}
+            style={{ border: 0 }}
+            allowFullScreen
+          ></iframe>
+        </>
+      )}
     </Box>
   );
 };
